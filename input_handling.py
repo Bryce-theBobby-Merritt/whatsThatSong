17c58b371d5c0192237919148adc8e95f0e1ef59
BRYCE MERRITT <merrittbryce@gmail.com>
2024-06-18 20:15:52 -0600

    implemented guessing game skeleton

--- input_handling.py
+++ input_handling.py
@@ -1,2 +1,8 @@
 def handle_input(user_input_playlist_link: str) -> str:
+    """
+    TODO implement input handling that checks:
+        - that is is a spotify link
+        - that it has a .com and a www.
+        - MORE
+    """
     return user_input_playlist_link
